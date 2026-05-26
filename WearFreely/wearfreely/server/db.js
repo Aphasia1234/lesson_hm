8577c5bfd4395d62822bcca44d831887c85d01b8
ray <3098350970@qq.com>
2026-05-26 17:22:04 +0800

    修改git提交规范

--- WearFreely/wearfreely/server/db.js
+++ WearFreely/wearfreely/server/db.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '?',
-  database: 'personpage'
+  host: process.env.PROCESS_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 module.exports = pool.promise();
